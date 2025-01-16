49ca2934b6d64fc399f5fae43646f50ff615f767
countzero1942 <countzero1942@yahoo.com>
2025-01-16 00:15:00 -0500

    Working on headings css and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,21 +6,49 @@ import {
 	mantineHtmlProps,
 } from "@mantine/core";
 import { theme } from "../theme";
-import "./global.css";
+import "@/styles/global.css";
 import TheApp from "./the-app";
+import { logobj } from "@/utils/log";
+import { log } from "console";
+import { Inter, Poppins, Lato } from "next/font/google";
+import clsx from "clsx";
 
 export const metadata = {
 	title: "Database Stuff",
 	description: "Database Stuff using Mantine with Next.js!",
 };
 
+const interFont = Inter({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--iu-font-family-inter",
+});
+
+const poppinsFont = Poppins({
+	subsets: ["latin"],
+	weight: ["600", "700", "800", "900"],
+	variable: "--iu-font-family-poppins",
+});
+
+const latoFont = Lato({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--iu-font-family-lato",
+});
+
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	log("theme");
+	log(theme);
 	return (
-		<html lang="en" {...mantineHtmlProps}>
+		<html
+			lang="en"
+			{...mantineHtmlProps}
+			className={clsx(poppinsFont.className, latoFont.className)}
+		>
 			<head>
 				<ColorSchemeScript />
 				<link rel="shortcut icon" href="/favicon.svg" />
--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,16 @@ export default async function HomePage() {
 				Hello, Next.js!
 			</Title>
 
-			<Title order={1}>Heading 1</Title>
-			<Title order={2}>Heading 2</Title>
-			<Title order={3}>Heading 3</Title>
-			<Title order={4}>Heading 4</Title>
-			<Title order={5}>Heading 5</Title>
-			<Title order={6}>Heading 6</Title>
+			<p>Some text</p>
 
 			<Divider variant="solid" my="lg" />
+
+			<h1>Heading 1</h1>
+			<h2>Heading 2</h2>
+			<h3>Heading 3</h3>
+			<h4>Heading 4</h4>
+			<h5>Heading 5</h5>
+			<h6>Heading 6</h6>
 		</article>
 	);
 }
